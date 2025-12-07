b34e135df874455372f4517063bebef6a5108bcb
ASHUTOSH KHILAR <ashutoshkhilar5@gmail.com>
2025-12-07 21:29:13 +0530

    Add stid field to Student model

--- app/models.py
+++ app/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Student(models.Model):
+    stid=models.CharField(max_length=100)
     stname=models.CharField(max_length=100)
     stage=models.IntegerField(max_length=100)
+    
